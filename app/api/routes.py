300a827cd454a465b8d21b334a60e43fce30c8fb
phum-raksakit <65050702@kmitl.ac.th>
2026-03-05 17:29:33 +0700

    Update routes.py

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,10 +1,10 @@
 """
 All API Routes
 """
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, Depends, UploadFile, File
 from app.models import RootResponse, ItemResponse, ItemRequest
 from fastapi.responses import JSONResponse
-from app.service import upload_handler
+from app.services import upload_handler
 
 router = APIRouter()
 
@@ -13,18 +13,9 @@ router = APIRouter()
 def read_root():
     return {"message": "World"}
 
-
-@router.get("/items/{item_id}", response_model=ItemResponse)
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
-
-
-@router.post("/items/", response_model=ItemResponse)
-def create_item(item: ItemRequest):
-    """Create a new item"""
-    return {"item_id": 1, "q": item.name}
-
 @router.post("/upload")
-async def upload(file: UploadFile = File(...)):
-    result = await upload_handler(file)
-    return JSONResponse(result)
+async def upload(
+    file: UploadFile = File(...),
+    service: upload_handler = Depends()
+):
+    return await service.save_file(file)
